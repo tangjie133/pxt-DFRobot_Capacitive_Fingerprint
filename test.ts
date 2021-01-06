1958e15f2bb4fb4cb08e1851ee3c445f2b913c04
TgJe <51323906+tangjie133@users.noreply.github.com>
2021-01-06 11:07:25 +0800

    更新 test.ts

--- test.ts
+++ test.ts
@@ -1,16 +1,20 @@
+input.onButtonPressed(Button.A, function () {
+    custom.delFingerprint(ID)
+})
+let ID = 0
 custom.isConnected()
 custom.delFingerprint(80)
 custom.ctrlLED(COLOR.eLEDYellow, MODE.eFastBlink, 2)
 custom.collectionFingerprint(10)
 custom.collectionFingerprint(10)
 custom.collectionFingerprint(10)
-let ID = custom.getEmptyID()
-custom.storeFingerprint(ID)
+ID = custom.getEmptyID()
 serial.writeValue("ID", ID)
+custom.storeFingerprint(ID)
 serial.writeValue("Number of users", custom.getEnrollCount())
 basic.forever(function () {
     custom.collectionFingerprint(10)
-    serial.writeValue("state", custom.search())
+    serial.writeValue("Match ID", custom.search())
     if (custom.verify(ID)) {
         custom.ctrlLED(COLOR.eLEDBlue, MODE.eFastBlink, 2)
     } else {
